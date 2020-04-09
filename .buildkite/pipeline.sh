11ecc9da4971c4607cc7fb65df356db062ada8c8
Gabriel P Samson <gabriel.samson@segment.com>
2020-04-09 11:02:21 -0700

    Finish upstream pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -3,9 +3,13 @@
 # exit immediately on failure, or if an undefined variable is used
 set -eu
 
-if [ ! -z "$(git diff --name-only upstream/master master)" ]; then
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$BRANCH" != "master" ] && [ ! -z "$(git diff --name-only upstream/master)" ]; then
+    git remote add upstream https://github.com/jsforce/jsforce.git || true
+    git fetch upstream
     cat .buildkite/upstream.yml
-fi
+else
     echo "steps:"
     echo "  - command: exit 0"
-end
+fi
